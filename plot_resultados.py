c845234b5afcb36ffda3db4edae081a2b34683a3
Vitor Batista Matos <vitorbatistamatos@gmail.com>
2025-10-31 21:21:24 -0300

    [31-10-2025] - Refatora plot para arquivo separado

---
+++ plot_resultados.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+def plot_results(p_resist_tempr, resist_compr, taxa_vol_erosao):
+    plt.figure(figsize=(7, 8))
+    plt.subplot(3, 1, 1)
+    plt.hist(p_resist_tempr, bins=50, color='skyblue')
+    plt.title('Distribuição da Perda de Resistência Após Altas Temperaturas')
+
+    plt.subplot(3, 1, 2)
+    plt.hist(resist_compr, bins=50, color='salmon')
+    plt.title('Distribuição da Resistência à Compressão')
+
+    plt.subplot(3, 1, 3)
+    plt.hist(taxa_vol_erosao, bins=50, color='lightgreen')
+    plt.title('Distribuição da Taxa de Erosão')
+
+    plt.tight_layout()
+    plt.show()
--- simulacao_resistencia_v2.py
+++ simulacao_resistencia_v2.py
@@ -1,10 +1,9 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 from perda_resistencia_temperatura import perda_resist_temp
 from calcula_resistencia_compressao import calcula_resist_compres
 from calcula_taxa_erosao import calcula_taxa_erosao
-
+from plot_resultados import plot_results
 
 n_sim = 100_000  # Número de simulações
 
@@ -24,20 +23,5 @@ print(f"Média da Resitência à Compressão = {np.mean(resist_compr):.3f}")
 print(f"Média da Taxa de Erosão = {np.mean(taxa_vol_erosao):.3f}")
 print(f"Desvio da Taxa de Erosão = {np.std(taxa_vol_erosao):.3f}")
 
-
 # Plot
-plt.figure(figsize=(7, 8))
-plt.subplot(3, 1, 1)
-plt.hist(p_resist_tempr, bins=50, color='skyblue')
-plt.title('Distribuição da Perda de Resistência Após Altas Temperaturas')
-
-plt.subplot(3, 1, 2)
-plt.hist(resist_compr, bins=50, color='salmon')
-plt.title('Distribuição da Resistência à Compressão em Altas Temperaturas')
-
-plt.subplot(3, 1, 3)
-plt.hist(taxa_vol_erosao, bins=50, color='lightgreen')
-plt.title('Distribuição da Taxa de Erosão')
-
-plt.tight_layout()
-plt.show()
+plot_results(p_resist_tempr, resist_compr, taxa_vol_erosao)
